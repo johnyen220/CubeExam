2a9caa0dce51a15915e305dea3f9cdde3a8f21f4
John <johnyen220@gmail.com>
2023-11-06 12:06:45 +0800

    Initial Commit

---
+++ CubeExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CubeExam
+//
+//  Created by 顏家揚 on 2023/11/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
